2bb42c01ce08afdb2633a352413dc9ce2c3567a6
Falcon <royale@kakao.com>
2022-08-21 22:22:22 +0900

    KeyCapSize

--- Assets/KeyboardPopCorn.cs
+++ Assets/KeyboardPopCorn.cs
@@ -6,13 +6,24 @@ public class KeyboardPopCorn : MonoBehaviour
 {
     public Transform spawnPosition;
     public GameObject PopCorn;
+    public int i = 0;
+    public int keyCount = 5;
+    public void repeatFunc()
+    {
+        SendMessage("Shake");
+        for (int i = 0; i < keyCount; i++)
+        {
+            GameObject Temp = Instantiate(PopCorn, spawnPosition.position, Quaternion.Euler(new Vector3(Random.Range(-45f, 45f), 0, 0)),spawnPosition);
+        }
+
+        if (i-- >= 0)
+            Invoke("repeatFunc", 0.3f);
+    }
 
     public void CreatePop()
     {
-        for(int i = 0; i < 5; i++)
-        {
-            GameObject Temp = Instantiate(PopCorn, spawnPosition.position, Quaternion.Euler(new Vector3(Random.Range(-45f, 45f), 0, 0)));
-        }
+        i = 10;
+        repeatFunc();
     }
 
 }
